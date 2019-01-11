5a3cd6abad1be34eb5bfd8423ef37c7092285686
Marc McKinley <marc@puppet.com>
2019-01-11 12:01:13 +0000

    Update workshop1.pp

--- site/bolt_student1/manifests/workshop1.pp
+++ site/bolt_student1/manifests/workshop1.pp
@@ -1,5 +1,15 @@
 class bolt_student1::workshop1 {
   include ::bolt_student1::puppet_agent
-  include ::bolt_student1::win_packages
+  
+   case $::kernel {
+    'windows': {
+      include ::bolt_student1::win_packages
+    }
+    'Linux':   {
+      include ::bolt_student1::lin_packages
+    }
+    default: {
+      fail('Unsupported operating system!')
+
 
 }
